fe7543b385e05bbe517886c1bff8564ea5f06996
Tabassum Alam <tabassumalam@Tabassums-MacBook-Pro.local>
2020-02-09 14:04:45 -0800

    week 5

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Flexers from './components/Flexers';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { angle: 0 }
+};
+
+  render() {
+    return (
+      <div className="App">
+        <Flexers />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Flexers.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class Flexers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            angle: 180,
+            time: 0,
+        }
+
+        this.setTime()
+    };
+
+    setTime() {
+        setInterval(() =>
+            this.setState({ time: this.state.time + 1 }), 1000);
+
+    }
+
+    getStyleTitle = () => {
+        return {
+            backgroundColor: "#add8e6",
+            color: "#ffcb",
+            fontStyle: "italic",
+            fontFamily: "Courier"
+
+        }
+    }
+
+    getStyleBody = () => {
+        return {
+            backgroundColor: "#ffcb",
+            color: "#999990",
+            fontStyle: "bold"
+        }
+    }
+
+    render() {
+        return (
+            <div >
+                <h1 style= {this.getStyleTitle()}> FLEXERS </h1>
+                <h2 style = {this.getStyleBody()}>
+                    your current angle: {this.state.angle} degrees 
+                    <br></br>
+                    time at current angle: {this.state.time} second
+                </h2>
+
+            </div>
+        );
+    }
+}
