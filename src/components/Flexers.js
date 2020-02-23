b685326d9454a7118a174df54075d175b747cced
Tabassum Alam <tabassumalam@Tabassums-MacBook-Pro.local>
2020-02-23 14:37:53 -0800

    week 7

--- src/components/Flexers.js
+++ src/components/Flexers.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+import Websocket from 'react-websocket';
 
 export default class Flexers extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            angle: 260,
+            angle: 0,
             time: 0,
         }
-
-        this.setTime()
     };
+
+    setAngle(angle) { // might have to parse data being read in 
+        this.setState({ angle, time: 0 }) // angle = new data read in , time = 0
+        this.setTime()  // starts counting time from 0
+    }
 
     setTime() {
         setInterval(() =>
             this.setState({ time: this.state.time + 1 }), 1000);
-
     }
 
     getStyleTitle = () => {
@@ -37,10 +40,12 @@ export default class Flexers extends Component {
 
     render() {
         return (
-            <div >
-                <h1 style= {this.getStyleTitle()}> FLEXERS </h1>
-                <h2 style = {this.getStyleBody()}>
-                    your current angle: {this.state.angle} degrees 
+            <div>
+                <Websocket url='http://localhost:8088/scale12345/'
+                    onMessage={this.setAngle.bind(this)} />
+                <h1 style={this.getStyleTitle()}> FLEXERS </h1>
+                <h2 style={this.getStyleBody()}>
+                    your current angle: {this.state.angle} degrees
                     <br></br>
                     time at current angle:  {(this.state.time)} seconds
                 </h2>
