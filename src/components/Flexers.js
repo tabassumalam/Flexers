7fa564439acaa409799e981c84ba2163fc76dce7
Tabassum Alam <tabassumalam@Tabassums-MacBook-Pro.local>
2020-02-29 17:48:43 -0800

    feb 29 end

--- src/components/Flexers.js
+++ src/components/Flexers.js
@@ -5,21 +5,33 @@ export default class Flexers extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            angle: 80,
+            angle: 0,
             time: 0,
         }
     };
 
     setAngle(angle) { // might have to parse data being read in 
-        this.setState({ angle, time: 0 }) // angle = new data read in , time = 0
-        this.setTime()  // starts counting time from 0
+        this.setState({ angle }) // angle = new data read in , time = 0
+        //this.setTime()  // starts counting time from 0
     }
 
     setTime() {
-        setInterval(() =>
-            this.setState({ time: this.state.time + 1 }), 1000);
+        setInterval(() => 
+            
+            this.setState({ time: this.state.time + 1}), 1000);
+        
     }
 
+    //useEffect(() => {
+     //   getAngle();
+    //}, []
+    //)
+    
+    getAngle = async () => {
+        const response = await fetch("http://localhost:8088/scale");
+        const data = await response.json();
+        this.setAngle(data.msg);
+    }
     getStyleTitle = () => {
         return {
             backgroundColor: "#add8e6",
@@ -43,12 +55,13 @@ export default class Flexers extends Component {
             
             <div>
                 <Websocket url='ws://localhost:8088/scale'
-                    onMessage={this.setAngle.bind(this)} />
+                    onMessage={this.setAngle.bind(this)}
+                    onMessage= {this.setTime()} />
                 <h1 style={this.getStyleTitle()}> FLEXERS </h1>
                 <h2 style={this.getStyleBody()}>
                     your current angle: {this.state.angle} degrees
                     <br></br>
-                    time at current angle:  {(this.state.time)} seconds
+                    time at current angle:  {(this.state.time )} seconds
                 </h2>
 
             </div>
